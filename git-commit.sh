371cb2140e65f396cb5a45e9bb673ba23860a367
sarahsantacruz <sarahsantacruz86@gmail.com>
2019-02-10 14:07:04 -0600

    first commit

---
+++ git-commit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+CURRENT_DIR=$(pwd)
+echo $CURRENT_DIR
+cd ~
+
+echo This script will push to your git repository.
+
+read -p "What directory would you like to push?  " directory
+read -p "Type your commit message here:  " message
+
+cd $directory
+
+git add .
+git commit -m "$message"
+git push
+
+cd $CURRENT_DIR
+
